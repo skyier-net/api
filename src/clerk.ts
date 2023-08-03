9f5ac06a1334786712bd2d05873e9ac3dd8396cb
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-03 13:22:18 +0300

    Clerk webhooks finished setup + handle User Create

--- src/clerk.ts
+++ src/clerk.ts
@@ -2,6 +2,9 @@ import type { WebhookEvent, UserJSON } from "@clerk/clerk-sdk-node";
 import { Webhook } from "svix";
 import bodyParser from "body-parser";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 import express from "express";
 const clerkRouter = express.Router();
 
@@ -10,7 +13,7 @@ const secret = "whsec_jm25hp7VmgpItc6owrivu8j0n+PaZh9q";
 clerkRouter.post(
   "/webhook",
   bodyParser.raw({ type: "application/json" }),
-  (req, res) => {
+  async (req, res) => {
     const payload = req.body;
     const headers = req.headers;
 
@@ -23,7 +26,23 @@ clerkRouter.post(
     }
 
     const data: UserJSON = msg.data;
-    console.log("MSG: " + data.primary_email_address_id);
+    switch (msg.type) {
+      case "user.created":
+        console.log("User created");
+        const user = await prisma.user.create({
+          data: {
+            id: data.id,
+            firstName: data.first_name,
+            lastName: data.last_name,
+            email: data.email_addresses.find(
+              (mail) => mail.id === data.primary_email_address_id
+            )!.email_address,
+            profilePictureUrl: data.profile_image_url,
+            createdAt: data.created_at,
+          },
+        });
+        console.log(user);
+    }
 
     res.json({});
   }
